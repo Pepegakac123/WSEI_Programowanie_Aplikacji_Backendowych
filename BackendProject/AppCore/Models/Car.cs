2d74c418a64762f53e92e4f527c02c5efe5489e5
Kacper Adamczyk <kacper.adamczyk@microsoft.wsei.edu.pl>
2026-02-28 17:21:43 +0100

    Added initial car class

--- BackendProject/AppCore/Models/Car.cs
+++ BackendProject/AppCore/Models/Car.cs
@@ -1,7 +1,11 @@
-﻿namespace AppCore.Models;
+﻿using AppCore.ValueObjects;
+
+namespace AppCore.Models;
 
 public class Car
 {
     public int Id { get; set;}
-    
+    public required PlateNumber PlateNumber { get; set; }
+    public DateTime Entry { get; set; }
+    public DateTime Exit { get; set; }
 }
