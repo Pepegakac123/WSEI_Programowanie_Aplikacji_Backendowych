ce23424dc4fe2f22464d31030c48a45cc126b75f
Kacper Adamczyk <kacpperr.adamczyk@gmail.com>
2026-03-14 17:27:45 +0100

    Update ParkingSession: Add VehicleId property to match UML diagram

--- BackendProject/AppCore/Models/ParkingSession.cs
+++ BackendProject/AppCore/Models/ParkingSession.cs
@@ -4,6 +4,7 @@ namespace AppCore.Models;
 
 public class ParkingSession : EntityBase
 {
+    public Guid VehicleId { get; set; }
     public required Vehicle Vehicle { get; set; }
     public string GateName { get; set; } = string.Empty;
     public DateTime EntryTime { get; set; }
